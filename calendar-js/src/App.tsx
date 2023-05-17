c7a02013465420ea3b3fa420025a2865c0aeb2c1
parya-jafari <parya@interaptix.com>
2023-05-17 13:50:53 -0400

    year calendar view

---
+++ calendar-js/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import React from "react";
+
+import YearView from "./features/year-view/YearView";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <YearView />
+      </header>
+    </div>
+  );
+}
+
+export default App;
